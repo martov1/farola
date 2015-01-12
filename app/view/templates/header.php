42679368a28df8327462d4321fa580b2a4da267b
martov1 <joacobrandan@gmail.com>
2015-01-11 22:45:33 -0300

    navbar, dealing with responsive design

---
+++ app/view/templates/header.php
@@ -0,0 +1,23 @@
+<div id="header"  >
+    <div class="bg_pattern">
+
+        <div class="container">
+
+            <!-- Logo -->
+            <div id="logo">
+                <h1><a href="#">La Farola</a></h1>
+                <span>Bar y restaurante</span>
+            </div>
+
+            <!-- Nav -->
+            <nav id="nav">
+                <ul>
+                    <li class="active"><a href="index.php">Pedi ahora!</a></li>
+                    <li><a href="threecolumn.php">Donde estamos</a></li>
+                    <li><a href="twocolumn1.php">Nuestro local</a></li>
+
+                </ul>
+            </nav>
+        </div>
+    </div>
+</div>
